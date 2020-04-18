896c524ba49fbad444998e59998de402e28cb868
keydon <keydon@gmail.com>
2020-04-18 02:01:59 +0200

    added some pictures from an old backup

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+   function bbcodes()
+   {
+      var win;
+      win = window.open("bbcodes.php", "bbcodes", "toolbar=no,width=540, height=300, directories=no, status=no, scrollbars=yes, resize=no, menubar=no");
+   }
